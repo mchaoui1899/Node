5c303a5c574bcab9e1ec10c40e819e892fd05df3
Chaoui <mchaoui018@gmail.com>
2026-08-13 22:22:22 +0100

    API

--- src/Data/data.ts
+++ src/Data/data.ts
@@ -1,3 +1,21 @@
+export const people = [
+  {
+    name: "Lena",
+    job: "Architect",
+    isRich: true
+  },
+  {
+    name: "Marcus",
+    job: "Barista",
+    isRich: false
+  },
+  {
+    name: "Ava",
+    job: "Software Engineer",
+    isRich: true
+  }
+]
+
 export const startups = [
   {
     id: 0,
