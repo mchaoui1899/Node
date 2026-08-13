c267583944e2e26297bff830cd0e771e9297b246
Chaoui <mchaoui018@gmail.com>
2026-08-13 18:54:04 +0100

    error

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -4,3 +4,25 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
   console.error(" Erreur :", err.message);
   res.status(500).json({ status: "error", message: err.message || "Erreur serveur" });
 };
+
+// 500 error
+//403 forbieden 
+
+//200 (OK) : Requête réussie.
+
+//201 (Created) : Ressource créée avec succès (ex: création de compte, ajout en BDD).
+
+//204 (No Content) : Succès, mais aucune donnée renvoyée (ex: suppression DELETE).
+
+//400 (Bad Request) : Données envoyées invalides ou manquantes (ex: champ formulaire obligatoire vide).
+
+//401 (Unauthorized) : Pas authentifié (tu n'es pas connecté / pas de token JWT). (Différent de 403 !)
+
+//404 (Not Found) : La route ou la ressource n'existe pas.
+
+//409 (Conflict) : Conflit de données (ex: email déjà existant en BDD).
+
+// 500 (Internal Server Error) : Bug serveur imprévu.
+
+// 502 / 503 : Serveur indisponible, en maintenance ou surcharge.
+//
