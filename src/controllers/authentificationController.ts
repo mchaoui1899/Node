4d35af7686f3e8e8338be91ef40decdf4fd95691
Chaoui <mchaoui018@gmail.com>
2026-09-05 21:06:23 +0100

    authSoumission

--- src/controllers/authentificationController.ts
+++ src/controllers/authentificationController.ts
@@ -1,10 +1,48 @@
 import validator from 'validator'
+import { Request, Response } from 'express';
+
 export async function registerUser (req : Request ,res : Response) {
 
+ let { name , email, username, password } = req.body
+
+  if (!name || !email || !username || !password) {
+
+    return res.status(400).json({ error: 'All fields are required.' })
+
+  }
+
+ name = name.trim()
+  email = email.trim()
+  username = username.trim()
+
+  if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
+
+    return res.status(400).json(
+      { error: 'Username must be 1–20 characters, using letters, numbers, _ or -.' }
+    )
+  }
+ if (!validator.isEmail(email)) {
+
+    return res.status(400).json({ error: 'Invalid email format' })
+
+  }
+ try {
+
+    
 
 
 
 
+ } catch (err : any) {
+
+    console.error('Registration error:', err.message);
+    res.status(500).json({ error: 'Registration failed. Please try again.' })
+
+  }
 
 
 }
+
+
+
+   
